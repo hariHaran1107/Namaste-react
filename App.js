ae831603d41a44a0d38f1f945af125859fb64c24
hariHaran1107 <ramesh.hari11@gmail.com>
2025-03-27 00:10:15 +0530

    igniting our own app!!

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";  
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Im an h1 tag"),
-    React.createElement("h2", {}, "Im an h2 tag"),
+    React.createElement("h1", { key: "1" }, "I'm an h1 tag"),
+    React.createElement("h2", { key: "2" }, "I'm an h2 tag"),
   ]),
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Im an h1 tag"),
-    React.createElement("h2", {}, "Im an h2 tag"),
+    React.createElement("h1", { key: "3" }, "I'm an h1 tag"),
+    React.createElement("h2", { key: "4" }, "I'm an h2 tag"),
   ])
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(parent); 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # React from very scratch
+
+# parcel
+- Created dev build
+- creates local server
+- HMR - Hot Module Replacement 
